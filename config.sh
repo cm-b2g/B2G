b075a86bcff7aebdc63a7f1644477a76df382c2a
Sai Karthik Karra <kskarthik@users.noreply.github.com>
2016-08-08 14:28:58 +0530

    test

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/cm-b2g/b2g-manifest"}
+GITREPO=${GITREPO:-"git://github.com/kskarthik/b2g-manifest"}
 BRANCH=${BRANCH:-master}
 
 while [ $# -ge 1 ]; do
